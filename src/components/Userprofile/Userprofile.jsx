0dae1a36082baa41c6a7d0950af96990a3679722
AlmubdyMutaikhan <mutaikhan.almubdy@gmail.com>
2025-03-27 15:06:30 +0500

    fix

--- src/components/Userprofile/Userprofile.jsx
+++ src/components/Userprofile/Userprofile.jsx
@@ -3,16 +3,16 @@ import './Userprofile.css';
 import UserProgressChart from '../ChemStruct/Chart';
 const UserProfile = () => {
     const userInfo = {
-        name: "Салтанат Кәрімқызы",
-        bio: "Химияға қызығушылық танытатын студент, энергетикалық химия саласында білімін тереңдетуде.",
+        name: "Яхия Салтанат Кенжеғалиқызы",
+        bio: "Т.Есетов атындағы 264 мектеп-лицей химия пәні мұғалімі",
         progressData: [
             { date: '2024-01-01', score: 75 },
             { date: '2023-02-01', score: 85 },
             { date: '2022-03-01', score: 95 }
         ],
         achievements: [
-            "National Chemistry Award 2023",
-            "Completed Advanced Organic Chemistry Course"
+            "Қорқыт ата атындағы институт магистранты",
+            "Еңбек өтілі: 9 жыл"
         ],
         favoriteElements: ["Оттегі", "Сутек", "Литий"],
         recentActivities: [
@@ -26,12 +26,9 @@ const UserProfile = () => {
             <h1>Пайдаланушы профилі</h1>
             <h2>{userInfo.name}</h2>
             <p>{userInfo.bio}</p>
+            <img height={500} width={'auto'} src='./image.png' />
             <div className="user-section">
-                <h3>Оқу Прогресі</h3>
-                <UserProgressChart data={userInfo.progressData} />
-            </div>
-            <div className="user-section">
-                <h3>Жетістіктер</h3>
+                <h3>Туралы</h3>
                 <ul>
                     {userInfo.achievements.map((achievement, index) => (
                         <li key={index}>{achievement}</li>
@@ -43,14 +40,6 @@ const UserProfile = () => {
                 <ul>
                     {userInfo.favoriteElements.map((element, index) => (
                         <li key={index}>{element}</li>
-                    ))}
-                </ul>
-            </div>
-            <div className="user-section">
-                <h3>Соңғы Қызметтер</h3>
-                <ul>
-                    {userInfo.recentActivities.map((activity, index) => (
-                        <li key={index}>{activity}</li>
                     ))}
                 </ul>
             </div>
